74d8f51c8999e51c367ae9eceaea32b12f066ef7
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2022-03-15 03:31:16 -0400

    parser: keep parse hints

--- src/parser.ts
+++ src/parser.ts
@@ -1,25 +1,70 @@
-export class ParseError {
+type Heap<a> = a | Array<Heap<a>>;
+
+function* flatten<a>(xs:Heap<a>) : Generator<a> {
+	if(Array.isArray(xs))
+		for(let x of xs)
+			yield* flatten(x);
+	else
+		yield xs;
+}
+
+export class ParseHint {
 	constructor(
 		readonly index:number,
-		readonly expected:Array<string>,
+		readonly hint:string,
 	) {}
+}
+
+export class ParseError {
+	constructor(
+		readonly hints:Heap<ParseHint>,
+	) {}
+	[Symbol.iterator]() {
+		return flatten(this.hints);
+	}
+	last() {
+		let result = { index: 0, hints: new Set() };
+		for(let hint of this) {
+			if(Number.isNaN(hint.index)) continue;
+			if(result.index < hint.index)
+				result = { index: hint.index, hints: new Set() };
+			if(result.index == hint.index)
+				result.hints.add(hint.hint);
+		}
+		return result;
+	}
+	message() {
+		let { index, hints: hints$ } = this.last();
+		let hints = Array.from(hints$);
+		switch(hints.length) {
+			case 1: return hints[0];
+			case 2: return `${hints[0]} or ${hints[1]}`;
+			default: return `${hints.slice(0,-1).join(', ')}, or ${hints[hints.length-1]}`;
+		}
+	}
 }
 
 export class ParseResult<a> {
 	constructor(
 		readonly index:number,
 		readonly value:a,
+		readonly hints:Heap<ParseHint>=[],
 	) {}
 }
 
 export type Parser<a> = (str:string, idx:number) => ParseResult<a>;
 
 // <?>
-export function label<a>(name:Array<string>|string, p:Parser<a>) : Parser<a> {
-	name = typeof name === 'string' ? [name] : name;
-	return recover(p, (err) => (str, idx) => {
-		throw new ParseError(err.index, name as Array<string>);
-	});
+export function label<a>(name:Heap<string>, p:Parser<a>) : Parser<a> {
+	let names = Array.from(flatten(name));
+	return function(str, idx) {
+		try {
+			return p(str, idx)
+		} catch (err) {
+			if(!(err instanceof ParseError)) throw err;
+			throw new ParseError(names.map((x) => new ParseHint(idx, x)))
+		}
+	}
 }
 
 // withRecovery
@@ -27,42 +72,49 @@ export function recover<a>(p:Parser<a>, f:(e:ParseError)=>Parser<a>) : Parser<a>
 	return function(str, idx) {
 		try {
 			return p(str, idx);
-		} catch(err) {
-			if(!(err instanceof ParseError)) throw err;
-			return f(err)(str, idx);
+		} catch(err1) {
+			if(!(err1 instanceof ParseError)) throw err1;
+			try {
+				let r = f(err1)(str, idx);
+				return new ParseResult(r.index, r.value, [r.hints, err1.hints])
+			} catch(err2) {
+				if(!(err2 instanceof ParseError)) throw err2;
+				throw new ParseError([err1.hints, err2.hints])
+			}
 		}
-	};
+	}
 }
 
 // notFollowedBy
 export function not<a>(p:Parser<a>) : Parser<null> {
 	return function(str, idx) {
+		let r : ParseResult<a>;
 		try {
-			p(str, idx);
+			r = p(str, idx);
 		} catch(err) {
 			if(!(err instanceof ParseError)) throw err;
-			return new ParseResult(idx, null);
+			return new ParseResult(idx, null, r.hints);
 		}
-		throw new ParseError(idx, []);
+		throw new ParseError(new ParseHint(idx, '<not>'));
 	};
 }
 
 // optional
 export function optional<a>(p:Parser<a>) : Parser<a|null> {
-	return recover(p, (err) => (str, idx) => new ParseResult(idx, null));
+	return recover(p, (err) => (str, idx) => new ParseResult(idx, null, err.hints));
 }
 
 // anySingle
 export function single(str:string, idx:number) : ParseResult<string> {
-	if(idx >= str.length) throw new ParseError(idx, ['any token']);
-	return new ParseResult(idx + 1, str[idx]);
+	if(idx >= str.length) throw new ParseError(new ParseHint(idx, 'any token'));
+	return new ParseResult(idx + 1, str[idx], []);
 }
 
 // mfilter
 export function filter<a>(p:Parser<a>, f:(x:a)=>boolean) : Parser<a> {
 	return function(str, idx) {
 		let r = p(str, idx);
-		if(!f(r.value)) throw new ParseError(idx, []);
+		if(!f(r.value)) throw new ParseError(new ParseHint(idx, '<filter>'));
 		return r;
 	}
 }
@@ -78,7 +130,7 @@ export function sepby<a,b>(p:Parser<a>, s:Parser<b>)
 // satisfy
 export function satisfy<a>(f:(x:string)=>boolean) : Parser<string> {
 	return function(str, idx) {
-		if(!f(str[idx])) throw new ParseError(idx, []);
+		if(!f(str[idx])) throw new ParseError(new ParseHint(idx, 'satisfy'));
 		return new ParseResult(idx + 1, str[idx]);
 	}
 }
@@ -87,7 +139,7 @@ export function satisfy<a>(f:(x:string)=>boolean) : Parser<string> {
 export function string<a>(s:string) : Parser<string> {
 	return function(str, idx) {
 		if(str.slice(idx, idx + s.length) != s)
-			throw new ParseError(idx, [JSON.stringify(s)]);
+			throw new ParseError(new ParseHint(idx, JSON.stringify(s)));
 		return new ParseResult(idx + s.length, s);
 	}
 }
@@ -115,11 +167,11 @@ export function nth(ps:Array<Parser<any>>, n:number) : Parser<any> {
 
 // <|>
 export function or<a>(x:Parser<a>, y:Parser<a>) : Parser<a> {
-	return recover(x, (err1) => recover(y, (err2) => {
-		if(err1.index > err2.index) throw err1;
-		if(err1.index < err2.index) throw err2;
-		throw new ParseError(err1.index,
-			[].concat.call(err1.expected, err2.expected));
+	return recover(x, (err1) => recover(function(str, idx) {
+		let r = y(str, idx);
+		return new ParseResult(r.index, r.value, [r.hints, err1.hints]);
+	}, (err2) => {
+		throw new ParseError([err1.hints, err2.hints]);
 	}));
 }
 
@@ -131,8 +183,8 @@ export function choice<a>(ps:Array<Parser<a>>) : Parser<a> {
 // <$>
 export function map<a,b>(p:Parser<a>, f:(x:a)=>b) : Parser<b> {
 	return function(str, idx) {
-		let {index, value} = p(str, idx);
-		return new ParseResult(index, f(value));
+		let r = p(str, idx);
+		return new ParseResult(r.index, f(r.value), r.hints);
 	};
 }
 
@@ -140,8 +192,15 @@ export function map<a,b>(p:Parser<a>, f:(x:a)=>b) : Parser<b> {
 export function then<a,b>(x:Parser<a>, y:Parser<b>) : Parser<{x:a, y:b}> {
 	return function(str, idx) {
 		let res1 = x(str, idx);
-		let res2 = y(str, res1.index);
-		return new ParseResult(res2.index, {x:res1.value, y:res2.value});
+		try {
+			let res2 = y(str, res1.index);
+			return new ParseResult(res2.index,
+				{x:res1.value, y:res2.value},
+				[res1.hints, res2.hints]);
+		} catch(err) {
+			if(!(err instanceof ParseError)) throw err;
+			throw new ParseError([err.hints, res1.hints]);
+		}
 	};
 }
 
@@ -158,8 +217,14 @@ export function second<a,b>(x:Parser<a>, y:Parser<b>) : Parser<b> {
 // >>=
 export function bind<a,b>(p:Parser<a>, f:(x:a)=>Parser<b>) : Parser<b> {
 	return function(str, idx) {
-		let r = p(str, idx);
-		return f(r.value)(str, r.index);
+		let r1 = p(str, idx);
+		try {
+			let r2 = f(r1.value)(str, r1.index);
+			return new ParseResult(r2.index, r2.value, [r1.hints, r2.hints]);
+		} catch(err) {
+			if(!(err instanceof ParseError)) throw err;
+			throw new ParseError([r1.hints, err.hints]);
+		}
 	};
 }
 
@@ -171,7 +236,7 @@ export function many<a>(p:Parser<a>) : Parser<Array<a>> {
 		while(true) {
 			let r = p(str, idx);
 			if(r.value == null)
-				return new ParseResult(idx, xs);
+				return new ParseResult(idx, xs, r.hints);
 			idx = r.index;
 			xs.push(r.value);
 		}
@@ -207,7 +272,7 @@ export function infixl<a>(term:Parser<a>, op:Parser<(x:a,y:a)=>a>) : Parser<a> {
 ////////////////////////////////////////////////////////////////////////////////
 
 export function eof(str:string, idx:number) : ParseResult<null> {
-	if(idx < str.length) throw new ParseError(idx, ['EOF']);
+	if(idx < str.length) throw new ParseError(new ParseHint(idx, '<EOF>'));
 	return new ParseResult(idx, null);
 }
 
@@ -223,7 +288,7 @@ export function regex(regex:RegExp) : Parser<RegExpExecArray> {
 	return function(str:string, idx:number) {
 		regex.lastIndex = idx;
 		let match = regex.exec(str);
-		if(!match) throw new ParseError(idx, [regex.toString()]);
+		if(!match) throw new ParseError(new ParseHint(idx, regex.toString()));
 		return new ParseResult(idx + match[0].length, match);
 	}
 }

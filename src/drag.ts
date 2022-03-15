c58d5ebbe18b01719e2306a806f78f6284e193f0
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2022-03-15 03:32:31 -0400

    drag: handle click

--- src/drag.ts
+++ src/drag.ts
@@ -3,6 +3,7 @@ export function drag(elem:HTMLElement,
 	update:(event:MouseEvent,x:number,y:number)=>void,
 	start:(event:MouseEvent,posn:{x:number,y:number})=>void=(e,p)=>{},
 	end:(event:MouseEvent,posn:{x:number,y:number})=>void=(e,p)=>{},
+	click:(event:MouseEvent,posn:{x:number,y:number})=>void=(e,p)=>{},
 ) : void {
 	let listener = {
 		move: null as (event:MouseEvent)=>void,
@@ -10,16 +11,21 @@ export function drag(elem:HTMLElement,
 	elem.addEventListener('mousedown', (event:MouseEvent) => {
 		if(listener.move !== null) return;
 		event.preventDefault();
+		event.stopImmediatePropagation();
 		start(event, posn);
 		posn.x -= event.clientX;
 		posn.y -= event.clientY;
+		let moved = false;
 		window.addEventListener('mouseup', listener.up = (event:MouseEvent) => {
 			if(listener === null) return;
 			event.preventDefault();
 			posn.x += event.clientX;
 			posn.y += event.clientY;
-			update(event, posn.x, posn.y);
-			end(event, posn);
+			if(moved) {
+				update(event, posn.x, posn.y);
+				end(event, posn);
+			} else
+				click(event, posn);
 			window.removeEventListener('mousemove', listener.move);
 			window.removeEventListener('mouseup', listener.up);
 			listener = { move: null, up: null };
@@ -27,6 +33,7 @@ export function drag(elem:HTMLElement,
 		window.addEventListener('mousemove', listener.move = (event:MouseEvent) => {
 			event.preventDefault();
 			update(event, posn.x + event.clientX, posn.y + event.clientY);
+			moved = true;
 		});
 	});
 }

c4093dd48510fd2327bdaa1770d7686c4b75f4db
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2022-03-15 03:38:33 -0400

    main: handle lists and options

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,18 @@ import { polynomial, fft } from './fft';
 import { rec } from './rec';
 import { calculate } from './calculator';
 import { Indexed, makeIndexed } from './util';
+
 import * as R from './response';
 import * as P from './polezero';
 import * as L from './list';
 import * as T from './root';
 
+(window as any).S = S;
 
+function trace(...xs:any) {
+	console.log(...xs);
+	return xs[xs.length - 1];
+}
 
 drag(document.getElementById('floaty-bar'), S.floaty.position, (event, x, y) => {
 	let elem = document.getElementById('floaty');
@@ -69,10 +75,82 @@ window.addEventListener('resize', (event:UIEvent) => {
 	R.plotLine();
 });
 
-function trace(...xs:any) {
-	console.log(...xs);
-	return xs[xs.length - 1];
-}
+let forms = document.forms.namedItem('options');
+
+(forms.elements.namedItem('resolution') as HTMLInputElement)
+	.addEventListener('input', (event:InputEvent) => {
+		let target = event.target as HTMLInputElement;
+		let value = Math.pow(2, parseInt(target.value));
+		S.option.resolution = value;
+		target.nextElementSibling.textContent = value.toString();
+		S.poles.real = new Float64Array(value);
+		S.poles.imag = new Float64Array(value);
+		S.zeros.real = new Float64Array(value);
+		S.zeros.imag = new Float64Array(value);
+		S.response.abs = new Float64Array(1 + (value >> 1));
+		R.plotAxisX();
+		S.recalculate.forEach((f) => f(S.poles));
+		S.recalculate.forEach((f) => f(S.zeros));
+	});
+
+T.create(S.option.gain,
+	(forms.elements.namedItem('gain') as HTMLInputElement).parentElement,
+	(event, root, node) => {
+		if(root.error !== null) return;
+		S.option.gain = root;
+		S.recalculate.forEach((f) => f(S.poles));
+	});
+
+T.create(S.option.frequency,
+	(forms.elements.namedItem('frequency') as HTMLInputElement).parentElement,
+	(event, root, node) => {
+		if(root.error !== null) return;
+		S.option.frequency = root;
+		R.plotAxisX();
+	});
+
+for(let snap of (forms.elements.namedItem('snap') as unknown as Array<HTMLInputElement>))
+	snap.addEventListener('change', (event:InputEvent) => {
+		let target = event.target as HTMLInputElement;
+		S.option.snap[snap.value as keyof typeof S.option.snap] = snap.checked;
+	});
+
+S.recalculate.push(function(roots) {
+	let target = forms.elements.namedItem('export') as HTMLInputElement;
+	let zs = S.zeros.roots.map((r) => C.conjugates(r.value)).flat();
+	let ps = S.poles.roots.map((r) => C.conjugates(r.value)).flat();
+	let Bs = [].join.call(polynomial(zs)(), ', ');
+	let As = [].join.call(polynomial(ps)(), ', ');
+	target.value = `B = [${Bs}]\nA = [${As}]\n` +
+		`zeros = [${zs.join(', ')}]\npoles = [${ps.join(', ')}]`
+});
+
+S.recalculate.push(function(roots:Roots) {
+	let list:HTMLElement = ({
+		zeros: document.getElementById('slide-zeros').querySelector('ul'),
+		poles: document.getElementById('slide-poles').querySelector('ul'),
+	} as Record<string,HTMLElement>)[roots.name]
+	function onchange(event:InputEvent, root:Root, node:HTMLElement) {
+		event.preventDefault();
+		let repr = node.querySelector('input');
+		let index = d3.select<HTMLElement,Indexed<Root>>(node).datum().index;
+		roots.roots[index] = root;
+		if(roots.roots.length - 1 === index && root.repr !== '' && root.error === null)
+			roots.roots.push(new Root("", new Complex(0, 0)));
+		if(root.repr === '')
+			roots.roots.splice(index, 1);
+		S.recalculate.forEach((f) => f(roots));
+		if(root.error === null)
+			repr.parentElement.nextElementSibling.querySelector('input').focus()
+	}
+	d3.select<HTMLElement,Array<Root>>(list)
+		.selectAll('li').data(roots.roots.map(makeIndexed))
+		.join((enter) => enter.append('li').each(
+				function(t) { T.create(t.value, this, onchange); }),
+			(update) => update.each(
+				function(t) { T.render(t.value, this as HTMLElement); }),
+			(exit) => exit.remove())
+});
 
 R.plotSize();
 R.plotAxisX();
